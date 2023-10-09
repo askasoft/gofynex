c03967f3cfa6f7b65660440e2e51ba192a447ca1
Frank Wang <yf.frank.wang@outlook.com>
2023-10-09 23:42:25 +0900

    console: use rich text

--- widgets/console.go
+++ widgets/console.go
@@ -3,7 +3,9 @@ package widgets
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/askasoft/pango/cog"
 )
 
 type Console struct {
@@ -11,7 +13,9 @@ type Console struct {
 
 	MaxLines int
 
-	vbox *fyne.Container
+	Segments cog.ArrayList[widget.RichTextSegment]
+
+	RichText *widget.RichText
 }
 
 func NewConsole(maxLines int) *Console {
@@ -19,25 +23,57 @@ func NewConsole(maxLines int) *Console {
 		MaxLines: maxLines,
 	}
 
-	c.vbox = container.NewVBox()
-	c.Scroll = container.NewVScroll(c.vbox)
+	c.RichText = widget.NewRichText()
+	c.RichText.Wrapping = fyne.TextWrapBreak
 
-	c.ExtendBaseWidget(c)
+	c.Scroll = container.NewVScroll(c.RichText)
 
 	return c
 }
 
 func (c *Console) Add(s string, imp widget.Importance) {
-	if len(c.vbox.Objects) > c.MaxLines {
-		c.vbox.Remove(c.vbox.Objects[0])
+	if c.Segments.Len() > c.MaxLines {
+		c.Segments.PollHead()
 	}
 
-	lbl := widget.NewLabel(s)
-	lbl.Importance = imp
-	lbl.Wrapping = fyne.TextWrapBreak
-	c.vbox.Add(lbl)
+	var color fyne.ThemeColorName
+	switch imp {
+	case widget.LowImportance:
+		color = theme.ColorNameDisabled
+	case widget.MediumImportance:
+		color = theme.ColorNameForeground
+	case widget.HighImportance:
+		color = theme.ColorNamePrimary
+	case widget.DangerImportance:
+		color = theme.ColorNameError
+	case widget.WarningImportance:
+		color = theme.ColorNameWarning
+	case widget.SuccessImportance:
+		color = theme.ColorNameSuccess
+	default:
+		color = theme.ColorNameForeground
+	}
+
+	c.Segments.Add(&widget.TextSegment{
+		Style: widget.RichTextStyle{
+			Alignment: fyne.TextAlignLeading,
+			ColorName: color,
+			Inline:    true,
+			TextStyle: fyne.TextStyle{},
+		},
+		Text: s,
+	})
+	c.RichText.Segments = c.Segments.Values()
+	c.RichText.Refresh()
+}
+
+func (c *Console) Refresh() {
+	c.RichText.Segments = c.Segments.Values()
+	c.RichText.Refresh()
+	c.Scroll.Refresh()
 }
 
 func (c *Console) Clear() {
-	c.vbox.RemoveAll()
+	c.Segments.Clear()
+	c.Refresh()
 }
